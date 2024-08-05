664d50447889f4e1ec58757cc4884a141e8eda02
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2024-08-05 19:16:26 -0300

    feat: added customer adapters and database

--- src/infrastructure/factories/ControllerFactory.js
+++ src/infrastructure/factories/ControllerFactory.js
@@ -1,11 +1,20 @@
 const ProductsController = require("../../adapters/api/ProductsController");
-const ProductManagement = require("../../core/use-cases/ProductManagement");
+const ProductManagement = require("../../core/products/use-cases/ProductManagement");
 const SequelizeProductRepository = require("../../adapters/database/SequelizeProductRepository");
+const CustomerController = require("../../adapters/api/CustomerController");
+const CustomerManagement = require("../../core/customers/use-cases/CustomerManagement");
+const SequelizeCustomerRepository = require("../../adapters/database/SequelizeCustomerRepository");
 
 module.exports = class ControllerFactory {
   static makeProductManagementController() {
     return new ProductsController(
       new ProductManagement(new SequelizeProductRepository())
+    );
+  }
+
+  static makeCustomerManagementController() {
+    return new CustomerController(
+      new CustomerManagement(new SequelizeCustomerRepository())
     );
   }
 };
