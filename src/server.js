53f8d1e8b97438358dca668a01cc69426163cf7b
RobsonArcoleze <Robson.arcoleze29@gmail.com>
2024-08-10 00:17:54 -0300

    fix: Dependency removed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { swaggerUi, swaggerDocs } = require('./config/swagger');
-const exampleRoutes = require('./routes/exampleRoutes');
 const ControllerFactory = require('./infrastructure/factories/ControllerFactory');
 
 const app = express();
@@ -8,7 +7,6 @@ const productManagementController = ControllerFactory.makeProductManagementContr
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api', exampleRoutes);
 app.use(productManagementController.getRouter());
 
 
