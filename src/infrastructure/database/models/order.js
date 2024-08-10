26b2f5667e125deecace957a748e1c8a0dfecef0
Thawan Fidelis <thawan.fidelis@gmail.com>
2024-08-10 18:57:57 -0300

    Add association orders<>customer

--- src/infrastructure/database/models/order.js
+++ src/infrastructure/database/models/order.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Order.hasMany(models.Item);
+      Order.belongsTo(models.Customer);
     }
   }
   Order.init(
