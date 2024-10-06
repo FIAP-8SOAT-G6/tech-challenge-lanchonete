d32e793940dff9fcbc4e9999bdacaa9b053177fc
vitorrafael <vrsilveira07@gmail.com>
2024-10-06 16:40:28 -0300

    build: untrack built files

--- build/core/products/entities/ProductCategory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ProductCategory = Object.freeze({
-    Lanche: "Lanche",
-    Acompanhamento: "Acompanhamento",
-    Bebida: "Bebida",
-    Sobremesa: "Sobremesa"
-});
-exports.default = ProductCategory;
