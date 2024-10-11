8ce921f04eb35a169f517fbd7508c793886e9dc0
vitorrafael <vrsilveira07@gmail.com>
2024-10-11 19:15:48 -0300

    chore: fix customer response

--- src/presenters/CustomerPresenter.ts
+++ src/presenters/CustomerPresenter.ts
@@ -1,25 +1,30 @@
 import CustomerDTO from "../core/customers/dto/CustomerDTO";
 
+export type CustomerResponse = {
+  id: number;
+  name: string;
+  email: string;
+  cpf: string;
+};
+
 export default class CustomerPresenter {
-  public static adaptCustomerData(customer: CustomerDTO | null): string {
-    if (!customer) return JSON.stringify({});
-    return JSON.stringify({
+  public static adaptCustomerData(customer: CustomerDTO | null): CustomerResponse {
+    if (!customer) return {} as CustomerResponse;
+    return {
       id: customer.id,
       name: customer.name,
       email: customer.email,
       cpf: customer.cpf
-    });
+    } as CustomerResponse;
   }
 
-  public static adaptCustomersData(customers: CustomerDTO[] | null): string {
-    if (!customers) return JSON.stringify({});
-    return JSON.stringify(
-      customers.map((customer) => ({
-        id: customer.id,
-        name: customer.name,
-        email: customer.email,
-        cpf: customer.cpf
-      }))
-    );
+  public static adaptCustomersData(customers: CustomerDTO[] | null): CustomerResponse[] {
+    if (!customers) return [];
+    return customers.map((customer) => ({
+      id: customer.id,
+      name: customer.name,
+      email: customer.email,
+      cpf: customer.cpf
+    })) as CustomerResponse[];
   }
 }
