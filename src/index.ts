5c87502b198b17295a5c0dabaab45f2b60afd835
Vítor Rafael Veeck da Silveira <vrsilveira07@gmail.com>
2024-10-12 13:58:21 -0300

    Feature - Arquitetura K8s (#55)
    * build: add k8s database
    
    * build: add k8s api
    
    * feat: add health endpoints
    
    * build: implements liveness and readiness probes
    
    * build: limits deployment resources
    
    * Add files via upload
    
    * chore: rename aws eks diagram
    
    * chore: rename files and use StatefulSet instead of Deployment for db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
+import { Application } from "express";
 import { sequelize } from "./infrastructure/database/models";
 import app from "./server";
 const PORT_SERVER = process.env.PORT_SERVER || 3000;
 
+function configureHealthRoutes(app: Application) {
+  app.get("/health/liveness", async function (_, res) {
+    return res.status(200).json({});
+  });
+
+  app.get("/health/readiness", async function (_, res) {
+    try {
+      await sequelize.authenticate();
+      return res.status(200).json({});
+    } catch (error) {
+      return res.status(500).json({});
+    }
+  });
+}
+
 async function init() {
   await sequelize.authenticate();
   await sequelize.sync();
+
+  configureHealthRoutes(app);
 
   const server = app.listen(PORT_SERVER, () => {
     console.log(`Server running on port ${PORT_SERVER}`), console.log(`Documentação da API disponível em http://localhost:${PORT_SERVER}/api-docs`);
