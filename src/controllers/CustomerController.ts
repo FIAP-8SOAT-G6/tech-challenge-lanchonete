9abcc3d992ad9f3b3e3cc4ce63965564897dbe19
Vítor Rafael Veeck da Silveira <vrsilveira07@gmail.com>
2024-10-12 13:57:57 -0300

    chore: fix customer response (#64)

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -3,16 +3,16 @@ import EmailValidatorAdapter from "../adapters/services/validators/EmailValidato
 import CustomerDTO from "../core/customers/dto/CustomerDTO";
 import { CustomersFactory } from "../factories/CustomersFactory";
 import { CustomerDataSource } from "../interfaces/DataSources";
-import CustomerPresenter from "../presenters/CustomerPresenter";
+import CustomerPresenter, { CustomerResponse } from "../presenters/CustomerPresenter";
 
 export default class CustomerController {
-  public static async createCustomer(customerDataSource: CustomerDataSource, customer: CustomerDTO): Promise<string> {
+  public static async createCustomer(customerDataSource: CustomerDataSource, customer: CustomerDTO): Promise<CustomerResponse> {
     const useCase = CustomersFactory.makeCreateCustomer(customerDataSource, new CPFValidatorAdapter(), new EmailValidatorAdapter());
     const createdCustomer = await useCase.create(customer);
     return CustomerPresenter.adaptCustomerData(createdCustomer);
   }
 
-  public static async findCustomerByCpf(customerDataSource: CustomerDataSource, cpf: string): Promise<string> {
+  public static async findCustomerByCpf(customerDataSource: CustomerDataSource, cpf: string): Promise<CustomerResponse> {
     const useCase = CustomersFactory.makeFindCustomerByCpf(customerDataSource);
     const foundCustomer = await useCase.findByCPF(cpf);
     return CustomerPresenter.adaptCustomerData(foundCustomer);
