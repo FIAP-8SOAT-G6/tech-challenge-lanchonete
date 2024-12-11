df380710f3b0cebada914fa87b8039de2ef700b7
vitorrafael <vrsilveira07@gmail.com>
2024-12-10 21:58:57 -0300

    chore: create ecr repository

--- infra/repository.tf
+++ infra/repository.tf
@@ -1,4 +1,7 @@
-
-provider "aws" {
-  region = var.regionDefault
+resource "aws_ecr_repository" "lanchonete-api-ecr" {
+  name                 = "lanchonete-api"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
