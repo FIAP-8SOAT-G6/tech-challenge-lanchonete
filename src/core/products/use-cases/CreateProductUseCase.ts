d6a0e9f89ae08a10a3981cb0011f454656f8e83f
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2025-01-11 14:18:49 -0300

    feat: added product and customer mappers

--- src/core/products/use-cases/CreateProductUseCase.ts
+++ src/core/products/use-cases/CreateProductUseCase.ts
@@ -1,35 +1,13 @@
-import Product from "../entities/Product";
 import ProductDTO from "../dto/ProductDTO";
 import CreateProduct from "../interfaces/CreateProduct";
 import ProductGateway from "../../interfaces/ProductGateway";
+import { ProductMapper } from "../mappers/ProductMapper";
 
 export default class CreateProductUseCase implements CreateProduct {
   constructor(private productGateway: ProductGateway) {}
 
   async createProduct(productDTO: ProductDTO): Promise<ProductDTO> {
-    const product = this.#toProductEntity(productDTO);
-    return await this.productGateway.createProduct(this.#toProductDTO(product));
-  }
-
-  #toProductDTO(productEntity: Product) {
-    return new ProductDTO({
-      id: productEntity.getId(),
-      name: productEntity.getName(),
-      category: productEntity.getCategory(),
-      description: productEntity.getDescription(),
-      price: productEntity.getPrice(),
-      images: productEntity.getImages()
-    });
-  }
-
-  #toProductEntity(productDTO: ProductDTO) {
-    return new Product({
-      id: productDTO.id!,
-      name: productDTO.name!,
-      category: productDTO.category!,
-      description: productDTO.description!,
-      price: productDTO.price!,
-      images: productDTO.images!
-    });
+    const product = ProductMapper.toProductEntity(productDTO);
+    return await this.productGateway.createProduct(ProductMapper.toProductDTO(product));
   }
 }
