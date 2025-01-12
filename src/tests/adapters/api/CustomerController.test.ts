f968fa4ffb29594f6a95d89ddceb0008e852539f
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2025-01-12 12:10:30 -0300

    feat: added tests to customer controller

---
+++ src/tests/adapters/api/CustomerController.test.ts
@@ -0,0 +1,147 @@
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import request from "supertest";
+import app from "../../../../src/server";
+import SequelizeCustomerDataSource from "../../../external/SequelizeCustomerDataSource";
+import ResourceNotFoundError from "../../../core/common/exceptions/ResourceNotFoundError";
+import sinon from "sinon";
+import CustomerDTO from "../../../core/customers/dto/CustomerDTO";
+import MissingPropertyError from "../../../core/common/exceptions/MissingPropertyError";
+import InvalidAttributeError from "../../../core/common/exceptions/InvalidAttributeError";
+import ResourceAlreadyExistsError from "../../../core/common/exceptions/ResourceAlreadyExistsError";
+
+chai.use(chaiAsPromised);
+
+describe("Customer API", () => {
+  let findByPropertiesStub: sinon.SinonStub;
+  let createStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    findByPropertiesStub = sinon.stub(SequelizeCustomerDataSource.prototype, "findByProperties");
+    createStub = sinon.stub(SequelizeCustomerDataSource.prototype, "create");
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should return the customer on successful customer registration", async () => {
+    const customer = {
+      name: "Bob",
+      cpf: "12345678909",
+      email: "test@mail.com"
+    };
+
+    const customerDTO = new CustomerDTO(customer);
+
+    const customerCreated = {
+      id: "1",
+      name: "Bob",
+      email: "test@mail.com",
+      cpf: "123.456.789-09"
+    };
+
+    findByPropertiesStub.resolves(undefined);
+    createStub.resolves(customerCreated);
+
+    const res = await request(app).post("/customers").send(customer);
+    expect(res.status).to.equal(201);
+    expect(res.body).to.deep.equal(customerCreated);
+    expect(findByPropertiesStub.calledOnce).to.be.true;
+    expect(findByPropertiesStub.calledOnceWith({ cpf: customer.cpf })).to.be.true;
+    expect(createStub.calledOnce).to.be.true;
+    expect(createStub.calledOnceWith(customerDTO)).to.be.true;
+  });
+
+  it("should return error message when there is missing information on the customer to register the customer", async () => {
+    const customer = { name: "Bob", cpf: "12345678909", email: "" };
+    const res = await request(app).post("/customers").send(customer);
+
+    expect(res.status).to.equal(400);
+    expect(res.body).to.deep.equal({ message: new MissingPropertyError("email").message });
+    expect(findByPropertiesStub.called).to.be.false;
+    expect(createStub.called).to.be.false;
+  });
+
+  it("should return error message when customer information is wrong to register the customer", async () => {
+    const customer = {
+      name: "Bob",
+      cpf: "123",
+      email: "test@mail.com"
+    };
+
+    const res = await request(app).post("/customers").send(customer);
+
+    expect(res.status).to.equal(400);
+    expect(res.body).to.deep.equal({ message: new InvalidAttributeError("cpf", customer.cpf).message });
+    expect(findByPropertiesStub.called).to.be.false;
+    expect(createStub.called).to.be.false;
+  });
+
+  it("should return error message when the customer is already registered", async () => {
+    const customer = { name: "Bob", cpf: "12345678909", email: "test@mail.com" };
+    const customerCreated = { ...customer, id: "1", cpf: "123.456.789-09" };
+
+    findByPropertiesStub.resolves(undefined);
+    createStub.resolves(customerCreated);
+    await request(app).post("/customers").send(customer);
+
+    findByPropertiesStub.resolves([customerCreated]);
+    const res = await request(app).post("/customers").send(customer);
+
+    expect(res.status).to.equal(400);
+    expect(res.body).to.deep.equal({
+      message: new ResourceAlreadyExistsError(ResourceAlreadyExistsError.Resources.Customer, "cpf", customer.cpf).message
+    });
+    expect(findByPropertiesStub.calledTwice).to.be.true;
+  });
+
+  it("should return error message when an error occurs to register the customer", async () => {
+    findByPropertiesStub.rejects();
+
+    const customer = {
+      name: "Bob",
+      cpf: "12345678909",
+      email: "test@mail.com"
+    };
+
+    const res = await request(app).post("/customers").send(customer);
+
+    expect(res.status).to.equal(500);
+    expect(res.body).to.deep.equal({ message: "Error" });
+    expect(findByPropertiesStub.calledOnce).to.be.true;
+  });
+
+  it("should find customer by cpf", async () => {
+    const customer = { name: "Bob", cpf: "12345678909", email: "test@mail.com" };
+    const customerCreated = { ...customer, id: "1", cpf: "123.456.789-09" };
+
+    findByPropertiesStub.resolves(undefined);
+    createStub.resolves(customerCreated);
+    await request(app).post("/customers").send(customer);
+
+    findByPropertiesStub.resolves([customerCreated]);
+    await request(app).post("/customers").send(customer);
+    const res = await request(app).get(`/customers/${customer.cpf}`);
+
+    expect(res.status).to.equal(200);
+    expect(res.body).to.deep.equal(customerCreated);
+  });
+
+  it("should return error message when client is not found", async () => {
+    const cpf = "12345678909";
+    const res = await request(app).get(`/customers/${cpf}`);
+
+    expect(res.status).to.equal(404);
+    expect(res.body).to.deep.equal({ message: new ResourceNotFoundError(ResourceNotFoundError.Resources.Customer, "cpf", cpf).message });
+    expect(findByPropertiesStub.called).to.be.true;
+    expect(createStub.called).to.be.false;
+  });
+
+  it("should return status 404 when the CPF is not informed for customer search", async () => {
+    const res = await request(app).get("/customers/");
+    expect(res.status).to.equal(404);
+    expect(findByPropertiesStub.called).to.be.false;
+    expect(createStub.called).to.be.false;
+  });
+});
